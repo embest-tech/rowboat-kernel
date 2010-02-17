6a698974d6907e92e2fb37d62dd8a293303af6ae
Steve Kondik <shade@discord.hmsinc.com>
2010-02-17 12:59:29 -0500

    Add USB_ANDROID_RNDIS_WCEIS option.
    Change-Id: I85973ebfcbfae6b401fb8b402842309c72655149
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -232,7 +232,11 @@ static void enable_adb(struct android_dev *dev, int enable)
 		*/
 		if (!strcmp(f->name, "rndis")) {
 			if (enable)
+#ifdef CONFIG_USB_ANDROID_RNDIS_WCEIS
+				dev->cdev->desc.bDeviceClass = USB_CLASS_WIRELESS_CONTROLLER;
+#else
 				dev->cdev->desc.bDeviceClass = USB_CLASS_COMM;
+#endif
 			else
 				dev->cdev->desc.bDeviceClass = USB_CLASS_PER_INTERFACE;
 		}
