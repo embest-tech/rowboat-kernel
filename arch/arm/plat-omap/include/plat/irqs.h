dd09245416018696b4dab637561852fe5c5a4161
Hemant Pedanekar <hemantp@ti.com>
2010-05-02 22:49:35 +0530

    TI816X: Add platform data for PCIe RC
    Add module base and IRQ resources. Also add memory/IO resources required for PCI
    enumeration.
    
    The device initialization code takes care of setting RC mode operation of PCIe
    module before registering the platform device. for RC.
    
    Note that we specify complete 2GB of DDR space to be configured as inbound
    region. This is required when booting Linux with memory holes as the inbound
    address for DMA would be anywhere depending upong the 'mem' arguments.
    
    Signed-off-by: Hemant Pedanekar <hemantp@ti.com>

--- arch/arm/plat-omap/include/plat/irqs.h
+++ arch/arm/plat-omap/include/plat/irqs.h
@@ -417,7 +417,16 @@
 #define TWL_IRQ_END		TWL6030_IRQ_END
 #endif
 
-#define NR_IRQS			TWL_IRQ_END
+/* MSI IRQs may get added for TI81XX */
+#define MSI_IRQ_BASE		TWL_IRQ_END
+#ifdef CONFIG_PCI_MSI
+#define MSI_NR_IRQS		32
+#else
+#define MSI_NR_IRQS		0
+#endif
+#define MSI_IRQ_END		(MSI_IRQ_BASE + MSI_NR_IRQS)
+
+#define NR_IRQS			MSI_IRQ_END
 
 #define TI816X_IH_GPIO_BASE	128
 
