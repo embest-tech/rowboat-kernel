c0c3e12a663b2530929b0b772f79a21a2ab9fc84
Vaibhav Hiremath <hvaibhav@ti.com>
2011-01-17 14:54:01 +0530

    OMAP3: SDRC: Add timing data for Hynix H8KDS0UN0MER-4EM
    Signed-off-by: Ranjith Lohithakshan <ranjithl@ti.com>

--- arch/arm/mach-omap2/board-omap3evm.c
+++ arch/arm/mach-omap2/board-omap3evm.c
@@ -47,6 +47,7 @@
 
 #include "mux.h"
 #include "sdram-micron-mt46h32m32lf-6.h"
+#include "sdram-hynix-h8kds0un0mer-4em.h"
 #include "hsmmc.h"
 
 #define OMAP3_EVM_TS_GPIO	175
@@ -630,7 +631,10 @@ static void __init omap3_evm_init_irq(void)
 	omap_board_config = omap3_evm_config;
 	omap_board_config_size = ARRAY_SIZE(omap3_evm_config);
 	omap2_init_common_infrastructure();
-	omap2_init_common_devices(mt46h32m32lf6_sdrc_params, NULL);
+	if (cpu_is_omap3630())
+		omap2_init_common_devices(h8kds0un0mer4em_sdrc_params, NULL);
+	else
+		omap2_init_common_devices(mt46h32m32lf6_sdrc_params, NULL);
 	omap_init_irq();
 }
 
