a043bb8c4cb8eff54539d799c6b7c887500c5535
Afzal Mohammed <afzal@ti.com>
2011-05-18 17:39:23 +0530

    AM335X: Define clockdomains
    Define clockdomains in per, wkup,
    mpu, rtc, gfx & cefuse powerdomains
    
    Signed-off-by: Afzal Mohammed <afzal@ti.com>

--- arch/arm/mach-omap2/clockdomains81xx.h
+++ arch/arm/mach-omap2/clockdomains81xx.h
@@ -20,6 +20,7 @@
 
 #include "cm.h"
 #include "cm81xx.h"
+#include "cmAM335x.h"
 #include "cm-regbits-81xx.h"
 
 /*
@@ -268,6 +269,190 @@ static struct clockdomain default_usb_816x_clkdm = {
 	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
 	.flags		= CLKDM_CAN_HWSUP_SWSUP,
 	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_TI816X),
+};
+
+/*
+ * AM335X only
+ */
+
+static struct clockdomain l4ls_am335x_clkdm = {
+	.name		= "l4ls_clkdm",
+	.pwrdm		= { .name = "per_pwrdm" },
+	.cm_inst	= AM335x_CM_PER_MOD,
+	.clkdm_offs	= AM335x_CM_PER_L4LS_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain l3s_am335x_clkdm = {
+	.name		= "l3s_clkdm",
+	.pwrdm		= { .name = "per_pwrdm" },
+	.cm_inst	= AM335x_CM_PER_MOD,
+	.clkdm_offs	= AM335x_CM_PER_L3S_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain l4fw_am335x_clkdm = {
+	.name		= "l4fw_clkdm",
+	.pwrdm		= { .name = "per_pwrdm" },
+	.cm_inst	= AM335x_CM_PER_MOD,
+	.clkdm_offs	= AM335x_CM_PER_L4FW_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain l3_am335x_clkdm = {
+	.name		= "l3_clkdm",
+	.pwrdm		= { .name = "per_pwrdm" },
+	.cm_inst	= AM335x_CM_PER_MOD,
+	.clkdm_offs	= AM335x_CM_PER_L3_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain l4hs_am335x_clkdm = {
+	.name		= "l4hs_clkdm",
+	.pwrdm		= { .name = "per_pwrdm" },
+	.cm_inst	= AM335x_CM_PER_MOD,
+	.clkdm_offs	= AM335x_CM_PER_L4HS_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain ocpwp_l3_am335x_clkdm = {
+	.name		= "ocpwp_l3_clkdm",
+	.pwrdm		= { .name = "per_pwrdm" },
+	.cm_inst	= AM335x_CM_PER_MOD,
+	.clkdm_offs	= AM335x_CM_PER_OCPWP_L3_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain icss_ocp_am335x_clkdm = {
+	.name		= "icss_ocp_clkdm",
+	.pwrdm		= { .name = "per_pwrdm" },
+	.cm_inst	= AM335x_CM_PER_MOD,
+	.clkdm_offs	= AM335x_CM_PER_ICSS_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain cpsw_125mhz_am335x_clkdm = {
+	.name		= "cpsw_125mhz_clkdm",
+	.pwrdm		= { .name = "per_pwrdm" },
+	.cm_inst	= AM335x_CM_PER_MOD,
+	.clkdm_offs	= AM335x_CM_PER_CPSW_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain lcdc_am335x_clkdm = {
+	.name		= "lcdc_clkdm",
+	.pwrdm		= { .name = "per_pwrdm" },
+	.cm_inst	= AM335x_CM_PER_MOD,
+	.clkdm_offs	= AM335x_CM_PER_LCDC_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain clk_24mhz_am335x_clkdm = {
+	.name		= "clk_24mhz_clkdm",
+	.pwrdm		= { .name = "per_pwrdm" },
+	.cm_inst	= AM335x_CM_PER_MOD,
+	.clkdm_offs	= AM335x_CM_PER_CLK_24MHZ_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain l4_wkup_am335x_clkdm = {
+	.name		= "l4_wkup_clkdm",
+	.pwrdm		= { .name = "wkup_pwrdm" },
+	.cm_inst	= AM335x_CM_WKUP_MOD,
+	.clkdm_offs	= AM335x_CM_WKUP_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain l3_aon_am335x_clkdm = {
+	.name		= "l3_aon_clkdm",
+	.pwrdm		= { .name = "wkup_pwrdm" },
+	.cm_inst	= AM335x_CM_WKUP_MOD,
+	.clkdm_offs	= AM335x_CM_L3_AON_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain l4_wkup_aon_am335x_clkdm = {
+	.name		= "l4_wkup_aon_clkdm",
+	.pwrdm		= { .name = "wkup_pwrdm" },
+	.cm_inst	= AM335x_CM_WKUP_MOD,
+	.clkdm_offs	= AM335x_CM_L4_WKUP_AON_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain mpu_am335x_clkdm = {
+	.name		= "mpu_clkdm",
+	.pwrdm		= { .name = "mpu_pwrdm" },
+	.cm_inst	= AM335x_CM_MPU_MOD,
+	.clkdm_offs	= AM335x_CM_MPU_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain l4_rtc_am335x_clkdm = {
+	.name		= "l4_rtc_clkdm",
+	.pwrdm		= { .name = "rtc_pwrdm" },
+	.cm_inst	= AM335x_CM_RTC_MOD,
+	.clkdm_offs	= AM335x_CM_RTC_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain gfx_l3_am335x_clkdm = {
+	.name		= "gfx_l3_clkdm",
+	.pwrdm		= { .name = "gfx_pwrdm" },
+	.cm_inst	= AM335x_CM_GFX_MOD,
+	.clkdm_offs	= AM335x_CM_GFX_L3_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain gfx_l4ls_gfx_am335x_clkdm = {
+	.name		= "gfx_l4ls_gfx_clkdm",
+	.pwrdm		= { .name = "gfx_pwrdm" },
+	.cm_inst	= AM335x_CM_GFX_MOD,
+	.clkdm_offs	= AM335x_CM_GFX_L4LS_GFX_CLKSTCTRL__1_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
+};
+
+static struct clockdomain l4_cefuse_am335x_clkdm = {
+	.name		= "l4_cefuse_clkdm",
+	.pwrdm		= { .name = "cefuse_pwrdm" },
+	.cm_inst	= AM335x_CM_CEFUSE_MOD,
+	.clkdm_offs	= AM335x_CM_CEFUSE_CLKSTCTRL_OFFSET,
+	.clktrctrl_mask	= TI81XX_CLKTRCTRL_MASK,
+	.flags		= CLKDM_CAN_SWSUP,
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_AM335X),
 };
 
 #endif /* CONFIG_ARCH_TI81XX */
