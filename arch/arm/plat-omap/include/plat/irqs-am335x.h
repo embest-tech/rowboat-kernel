887d5cf65d48ae5180ad19435034b62b012cba42
Afzal Mohammed <afzal@ti.com>
2011-05-18 17:39:23 +0530

    AM335X: Add license for IRQ header
    Add GPLv2 license for IRQ header file
    
    Reported-by: Hebbar, Gururaja <gururaja.hebbar@ti.com>
    Signed-off-by: Afzal Mohammed <afzal@ti.com>

--- arch/arm/plat-omap/include/plat/irqs-am335x.h
+++ arch/arm/plat-omap/include/plat/irqs-am335x.h
@@ -1,3 +1,18 @@
+/*
+ * AM335X interrupts.
+ *
+ * Copyright (C) 2011 Texas Instruments, Inc. - http://www.ti.com/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #ifndef __ARCH_ARM_MACH_OMAP2_AM335x_IRQS_H
 #define __ARCH_ARM_MACH_OMAP2_AM335x_IRQS_H
 
