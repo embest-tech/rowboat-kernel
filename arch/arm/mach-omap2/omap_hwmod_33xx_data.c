b9fae20560dc90ba5c4f4c10242221a7b4e3662d
Vaibhav Hiremath <hvaibhav@ti.com>
2011-10-25 14:45:48 +0530

    Revert "ARM:omap3:am33xx: HACK for bypassing RTC hwmod"
    This reverts commit 24c28fd81b124665e77b3b096fd1fbc4b50a9ab7.

--- arch/arm/mach-omap2/omap_hwmod_33xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_33xx_data.c
@@ -2447,9 +2447,7 @@ static __initdata struct omap_hwmod *am33xx_hwmods[] = {
 	/* ocpwp class */
 	&am33xx_ocpwp_hwmod,
 	/* rtc class */
-#if 0
 	&am33xx_rtc_hwmod,
-#endif
 	/* sha0 class */
 	&am33xx_sha0_hwmod,
 	/* smartreflex class */
